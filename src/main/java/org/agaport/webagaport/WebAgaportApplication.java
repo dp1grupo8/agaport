adc3cbf8fa60ea76a2a941d86b9f6cb4c41f053b
bvez <bruno_espezua@hotmail.com>
2019-04-24 17:45:42 -0500

    primer commit

---
+++ src/main/java/org/agaport/webagaport/WebAgaportApplication.java
@@ -0,0 +1,13 @@
+package org.agaport.webagaport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebAgaportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebAgaportApplication.class, args);
+	}
+
+}
