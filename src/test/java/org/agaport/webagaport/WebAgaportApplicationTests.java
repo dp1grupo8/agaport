0c24568b585614055578bc20b63829acaf6e2cc5
bvez <bruno_espezua@hotmail.com>
2019-05-18 16:08:31 -0500

    se agrega carpeta test

---
+++ src/test/java/org/agaport/webagaport/WebAgaportApplicationTests.java
@@ -0,0 +1,16 @@
+package org.agaport.webagaport;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WebAgaportApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
