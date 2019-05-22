5311f03073d1c74d3cb66eb4766c11990752fdd5
bvez <bruno_espezua@hotmail.com>
2019-05-21 19:55:53 -0500

    modulo usuarios sin conexion

--- FRONTEND/src/app/pages/AGAPORT_gestion/Usuarios/usuarios.module.js
+++ FRONTEND/src/app/pages/AGAPORT_gestion/Usuarios/usuarios.module.js
@@ -11,11 +11,19 @@
           url: '/usuarios',
           title: 'Usuarios',
           templateUrl: 'app/pages/AGAPORT_gestion/Usuarios/usuarios.html',
-          //controller: 'ProfilePageCtrl',
+          controller: 'UsuariosCtrl',
           sidebarMeta: {
             icon: 'ion-android-laptop',
             order: 400,
           },
+        }).state('agaport_gestion.usuarios.nuevo', {
+          url: '/nuevo',
+          title: 'Nuevo usuario',
+          templateUrl: 'app/pages/AGAPORT_gestion/Usuarios/usuarios_nuevo.html',
+        }).state('agaport_gestion.usuarios.modificar', {
+          url: '/modificar',
+          title: 'Modificar usuario',
+          templateUrl: 'app/pages/AGAPORT_gestion/Usuarios/usuarios_modificar.html',
         });
   }
 
