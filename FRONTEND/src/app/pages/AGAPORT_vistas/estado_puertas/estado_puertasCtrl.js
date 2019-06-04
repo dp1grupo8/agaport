653c15957a922df998201e03e5e09a94deac285d
AMasudaN <masuda.a@pucp.pe>
2019-06-04 01:59:06 -0500

    colores de las puertas e icono de avion terminados

--- FRONTEND/src/app/pages/AGAPORT_vistas/estado_puertas/estado_puertasCtrl.js
+++ FRONTEND/src/app/pages/AGAPORT_vistas/estado_puertas/estado_puertasCtrl.js
@@ -29,28 +29,81 @@
         *      -aerolinea
         * -clase: clase del vuelo (string)
     */
+    // $('document').ready(function () {
+    //   var div_zonas = document.getElementsByClassName("zona");
+    //   for (var i = 0; i < div_zonas.length; i++) {
+    //     div_zonas[i].style.backgroundColor = "#E9E9E9";
+    //   }
+    //   var div_gates = document.getElementsByClassName("gate");
+    //   for (var i = 0; i < div_gates.length; i++) {
+    //     div_gates[i].style.backgroundColor = "#E9E9E9";
+    //   }
+    // });
     $http.get('/app/pages/AGAPORT_vistas/estado_puertas/data/puertas.json').then(function successCallback(response) {
+      var div_zonas = document.getElementsByClassName("zona");
+      for (var i = 0; i < div_zonas.length; i++) {
+        div_zonas[i].style.backgroundColor = "#E9E9E9";
+      }
+      var div_gates = document.getElementsByClassName("gate");
+      for (var i = 0; i < div_gates.length; i++) {
+        div_gates[i].style.backgroundColor = "#E9E9E9";
+      }
       $scope.puertas = response.data;
-      var puertas = $scope.puertas
-      console.log(puertas);
-      //no itera objetos
-      for (var puerta in puertas) {
-        var tipo = '';
-        console.log(puerta)
-        // if(puerta.tipo = 0){
-        //   tipo = 'gate';
-        // }
-        // var divId = tipo + '-' + puerta.idPuerta;
-        // if (puerta.estado = 1) {
-        //   document.getElementById(divId).style.backgroundColor = "blue"
-        // }
+      var puertas = $scope.puertas;
+      var i;
+      for (i = 0; i < puertas.length; i++) {
+        var puerta = puertas[i].Puerta;
+        var tipo;
+        if (puerta.tipo == 0) {
+          tipo = 'gate';
+        } else {
+          tipo = 'zona';
+        }
+        var divId = tipo + '-' + puerta.idPuerta;
+        if (puerta.estado == 1) {
+          document.getElementById(divId).style.backgroundColor = "#FFFFFF"
+        }
       }
     }, function errorCallback(response) {
-      console.log(response);
+      // console.log(response);
     });
     $http.get('/app/pages/AGAPORT_vistas/estado_puertas/data/vuelos-llegada.json').then(function successCallback(response) {
+      var div_zonas = document.getElementsByClassName("zona");
+      for (var i = 0; i < div_zonas.length; i++) {
+        div_zonas[i].style.backgroundColor = "#E9E9E9";
+      }
+      var div_gates = document.getElementsByClassName("gate");
+      for (var i = 0; i < div_gates.length; i++) {
+        div_gates[i].style.backgroundColor = "#E9E9E9";
+      }
       $scope.vuelos = response.data;
-      console.log(response.data);
+      var vuelos = $scope.vuelos;
+      var i;
+      for (i = 0; i < vuelos.length; i++) {
+        var puerta = vuelos[i].Puerta;
+        var tipo;
+        if (puerta.tipo == 0) {
+          tipo = 'gate';
+        } else {
+          tipo = 'zona';
+        }
+        var divId = tipo + '-' + puerta.idPuerta;
+        if (puerta.estado == 2) {
+          var src = document.getElementById(divId);
+          src.style.backgroundColor = "#F1C232";
+          //agregar icono de avion          
+          var img = document.createElement("img");
+          var width = Math.floor(src.offsetWidth*0.5);
+          console.log(width);
+          img.width = width;
+          img.height = width;
+          img.src = "/../../../../assets/pictures/aiga_departingflights-512.png";          
+
+          src.appendChild(img);
+        } else {
+          document.getElementById(divId).style.backgroundColor = "#F1C232";
+        }
+      }
     }, function errorCallback(response) {
       console.log(response);
     });
